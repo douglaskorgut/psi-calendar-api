a297574609a7e98e806ec1fbea04102b5656b8c2
douglaskorgut <douglaskorgutt@gmail.com>
2021-09-07 12:28:11 -0300

    test: ensure authenticate route returns jwt

---
+++ src/test/java/com/psi/calendar/api/controllers/AuthControllerTests.java
@@ -0,0 +1,56 @@
+package com.psi.calendar.api.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.psi.calendar.api.models.dto.auth.AuthRequestDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    public void init()
+    {
+        //Init MockMvc Object and build
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void itShouldReturnJwt() throws Exception {
+        var requestBody = AuthRequestDTO.builder().username("foo").password("foo").build();
+        var jsonRequestBody = mapper.writeValueAsString(requestBody);
+
+        this.mockMvc.perform(
+                post("/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(jsonRequestBody)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(
+                        content().string(containsString("jwt"))
+                );
+    }
+}
